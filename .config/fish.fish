2a2da4f1ac8a909dbcfc5864a7f59fa16a8449fb
Maycon Jesus <contato@mayconjesus.dev>
2025-08-19 14:16:12 -0300

    Update fish.fish

--- .config/fish.fish
+++ .config/fish.fish
@@ -5,13 +5,17 @@ end
 alias t="nohup ptyxis --working-directory . --tab >/dev/null 2>&1 &"
 alias cd="z"
 alias ls="eza --icons always --color always --long --git --no-filesize --no-time --no-user --no-permissions"
-
+alias gpt="mj bk o gpt"
+alias yt="mj bk o yt"
+alias copilot="mj bk o copilot"
+alias open="mj bk o"
 fish_add_path /usr/local/go/bin
 fish_add_path $HOME/bins
 
-starship init fish | source
+
 fzf --fish | source
 zoxide init fish | source
+starship init fish | source
 
 export FZF_CTRL_T_OPTS="
     --style full
